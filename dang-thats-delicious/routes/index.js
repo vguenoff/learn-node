fe5541732af7766898734a3aa7ea918bbc49bfed
Vasil Genov <vasil.genov@ad.mentormate.bg>
2018-02-22 16:18:49 +0200

    templating

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,13 +3,19 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  const wes = { name: 'Wes', age: 100, cool: true };
+  // const wes = { name: 'Wes', age: 100, cool: true };
   // res.send('Hey it works!');
   // res.json(wes);
 
   // http://localhost:7777/?name=wes&age=200&cool=true
   // res.send(req.query.name)
-  res.json(req.query)
+  // res.json(req.query)
+
+  // http://localhost:7777/?name=wes
+  res.render('hello', {
+    name: 'wes',
+    dog: req.query.dog,
+  });
 });
 
 router.get('/reverse/:name', (req, res) => {
