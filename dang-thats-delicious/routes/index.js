44379ce328af8fdc5efc6f5e8b84c908a2491ccb
vguenoff <vgenoff@gmail.com>
2018-02-27 09:50:09 +0200

    MVC basics

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,22 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  // const wes = { name: 'Wes', age: 100, cool: true };
-  // res.send('Hey it works!');
-  // res.json(wes);
-
-  // http://localhost:7777/?name=wes&age=200&cool=true
-  // res.send(req.query.name)
-  // res.json(req.query)
-
-  // http://localhost:7777/?name=wes
-  res.render('hello', {
-    name: 'wes',
-    dog: req.query.dog,
-  });
-});
+router.get('/', storeController.homePage);
 
 router.get('/reverse/:name', (req, res) => {
   // res.send('it works');
