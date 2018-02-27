da5da024c113b4efc4c1a30d867e0457d04f3d0e
vguenoff <vgenoff@gmail.com>
2018-02-27 10:57:20 +0200

    middleware basics

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,15 +3,6 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', storeController.homePage);
-
-router.get('/reverse/:name', (req, res) => {
-  // res.send('it works');
-
-  // http://localhost:7777/reverse/wes
-  const reversed = [...req.params.name].reverse().join('');
-  res.send(reversed)
-
-});
+router.get('/', storeController.myMiddleware, storeController.homePage);
 
 module.exports = router;
